56b8a936c7d8f5da3645f13a83c1d64f16d9b010
tabishadnan <tabishadnan000@gmail.com>
2020-12-23 03:15:56 +0500

    Updates

---
+++ precache-manifest.01e6230029594c9cc96f8ef4f01218f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2814fd3c960a280363a65fa35a0abae2",
+    "url": "/react-dice-exercise/index.html"
+  },
+  {
+    "revision": "aade4639ee36a19180a6",
+    "url": "/react-dice-exercise/static/css/main.485bfef5.chunk.css"
+  },
+  {
+    "revision": "a3780b2c85a0aa9f2cda",
+    "url": "/react-dice-exercise/static/js/2.d4fc85f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-dice-exercise/static/js/2.d4fc85f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aade4639ee36a19180a6",
+    "url": "/react-dice-exercise/static/js/main.8ca9921d.chunk.js"
+  },
+  {
+    "revision": "4d45c590f8e1f3574434",
+    "url": "/react-dice-exercise/static/js/runtime-main.a78e6ac5.js"
+  }
+]);
